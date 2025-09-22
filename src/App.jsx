3fbd99a2e6094fa059c1d8c335cbbc636c7f207c
soniagkalliath <soniageorge111@gmail.com>
2025-09-22 11:13:00 +0530

    style updated as tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { Button } from 'react-bootstrap'
+import Preloader from './components/Preloader'
+import Auth from './pages/Auth'
+import Pnf from './pages/Pnf'
+import Home from './users/pages/Home'
+import AllBooks from './users/pages/AllBooks'
+import ViewBook from './users/pages/ViewBook'
+import Profile from './users/pages/Profile'
+import Careers from './users/pages/Careers'
+import Contact from './users/pages/Contact'
+import DashboardAdmin from "./admin/pages/DashboardAdmin";
+import CareerAdmin from "./admin/pages/CareerAdmin";
+import ResourceAdmin from "./admin/pages/ResourceAdmin";
+import SettingAdmin from "./admin/pages/SettingAdmin";
 
 function App() {
 
+  const [loading,setLoading] = useState(true)
+
+  useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false)
+    }, 7000);
+  },[])
+
   return (
     <>
-    <FontAwesomeIcon icon={faInstagram} />  bookstore
-    <Button variant="primary">Primary</Button>
+      <Routes>
+        <Route path='/' element={loading?<Preloader/>:<Home/>} />
+        <Route path='/login' element={<Auth/>}/>
+        <Route path='/register' element={<Auth register/>}/>
+        <Route path='/all-books' element={<AllBooks/>}/>
+        <Route path='/books/:id/view' element={<ViewBook/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/careers' element={<Careers/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/admin-dashboard' element={loading?<Preloader/>:<DashboardAdmin/>} />
+        <Route path='/admin-resources' element={<ResourceAdmin/>}/>
+        <Route path='/admin-careers' element={<CareerAdmin/>}/>
+        <Route path='/admin-settings' element={<SettingAdmin/>}/>
+
+        <Route path='/*' element={<Pnf/>}/>
+      </Routes>
     </>
   )
 }
--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -1,8 +1,14 @@
+import { faFacebook, faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      header
+    </>
   )
 }
 
